5b09320793644c243673ef12e0a3ee59f90c9383
DanielRasho <danirayoroldan@gmail.com>
2024-04-17 12:12:22 -0600

    Feat: Create theme object, for storying color data.

---
+++ constants/theme.js
@@ -0,0 +1,25 @@
+const Theme = Object.freeze({
+  colors: {
+    white: "#FFFFFF",
+    grey: "#8D8D8D",
+    blue: "",
+    darkBlue: "",
+    aqua: "#6AB1B5",
+    darkAqua: "#448B8C",
+    green: "#6FBA3B",
+    darkGreen: "#4F9C2F",
+    red: "#E6333C",
+    darkRed: "#B52C41",
+  },
+  fontSizes: {
+    display: 24,
+    title: 20,
+    text: 15,
+    tag: 10,
+    buttonBig: 20,
+    buttonSmall: 16,
+  },
+  fontStyles: {
+    bold: "bold",
+  },
+});
