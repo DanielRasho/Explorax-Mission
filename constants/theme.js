dc3760746431c621d3a5377d170bb00b7e26c235
DanielRasho <danirayoroldan@gmail.com>
2024-04-21 12:35:14 -0600

    Fix: Theme constants

--- constants/theme.js
+++ constants/theme.js
@@ -2,24 +2,30 @@ const Theme = Object.freeze({
   colors: {
     white: "#FFFFFF",
     grey: "#8D8D8D",
-    blue: "",
-    darkBlue: "",
+    blue: "#1f4c8b",
+    darkBlue: "#132f51",
     aqua: "#6AB1B5",
     darkAqua: "#448B8C",
     green: "#6FBA3B",
     darkGreen: "#4F9C2F",
     red: "#E6333C",
     darkRed: "#B52C41",
+    orange: "#ec8a34",
+    darkOrange: "#e85729",
+    skyBlue: "#9bdafa",
+    darkSkyBlue: "#54a0c4",
   },
   fontSizes: {
-    display: 24,
+    display: 40,
     title: 20,
-    text: 15,
+    text: 20,
     tag: 10,
-    buttonBig: 20,
+    buttonBig: 40,
     buttonSmall: 16,
   },
-  fontStyles: {
+  fontWeight: {
     bold: "bold",
   },
 });
+
+export { Theme };
