d0dded99226528b53c07d30945dade3cf03e0406
DanielRasho <danirayoroldan@gmail.com>
2024-04-21 12:41:04 -0600

    Feat: Update app layout to display bottomNavegation and topstatus components

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,10 +1,24 @@
 import React, { useState } from "react";
 import { Slot } from "expo-router";
-import { View, Text, ImageBackground, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  ImageBackground,
+  StyleSheet,
+  Image,
+  Dimensions,
+} from "react-native";
 import { StatusBar } from "expo-status-bar";
-import BottomNavBar from "../components/BottomNavMenu";
+import BottomNavBar from "../components/molecules/BottomNavMenu";
+import GameStatusBar from "../components/molecules/GameStatusBar";
 
 const background = require("../assets/images/spaceBackground.png");
+const circle1 = require("../assets/images/menu/circle2.png");
+const circle2 = require("../assets/images/menu/circle1.png");
+const circle3 = require("../assets/images/menu/circle4.png");
+
+const windowWidth = Dimensions.get("window").width;
+const windowHeight = Dimensions.get("window").height;
 
 export default function GameLayout() {
   const [hello, setHello] = useState("Hello?");
@@ -14,12 +28,20 @@ export default function GameLayout() {
       resizeMode="cover"
       style={styles.background}
     >
-      <View>
-        <Text>hello</Text>
+      <View style={styles.bottomCircleContainer}>
+        <View style={[styles.circle, styles.circle1]}>
+          <Image source={circle1} style={styles.circleImage} />
+        </View>
+        <View style={[styles.circle, styles.circle2]}>
+          <Image source={circle2} style={styles.circleImage} />
+        </View>
+        <View style={[styles.circle, styles.circle3]}>
+          <Image source={circle3} style={styles.circleImage} />
+        </View>
       </View>
       <Slot />
       <BottomNavBar />
-
+      <GameStatusBar />
       <StatusBar hidden />
     </ImageBackground>
   );
@@ -28,6 +50,36 @@ export default function GameLayout() {
 const styles = StyleSheet.create({
   background: {
     flex: 1,
+    justifyContent: "space-between",
+    width: "100%",
+  },
+  bottomCircleContainer: {
+    position: "absolute",
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+  },
+  circle: {
+    position: "absolute",
+    width: "100%",
+    height: "45%",
+    overflow: "hidden",
     justifyContent: "center",
+    alignItems: "center",
+  },
+  circle1: {
+    bottom: -(windowHeight * 0.1),
+  },
+  circle2: {
+    bottom: -(windowHeight * 0.2),
+  },
+  circle3: {
+    bottom: -(windowHeight * 0.25),
+  },
+  circleImage: {
+    flex: 1,
+    resizeMode: "center",
+    height: undefined,
   },
 });
